8a76508a1ab4afe7d79cd284a0becf708937da5c
Hideto Ando <39573967+hideto2112@users.noreply.github.com>
2019-03-01 07:39:21 +0000

    21.CSSフレームワークを適用し、アプリケーション完成

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,8 +2,13 @@
 <html>
     <head>
         <title>Taks List</title>
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+        <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
+        <link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
     </head>
     <body>
-        @yield('content')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
